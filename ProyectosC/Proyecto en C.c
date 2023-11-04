1aded4a649caa7a61e96b6617b78ac0525405ce8
Jorge Fernández Varela Gómez <149905375+Jorge-Fernandez-Varela@users.noreply.github.com>
2023-11-04 23:07:41 +0000

    2

--- ProyectosC/Proyecto en C.c
+++ ProyectosC/Proyecto en C.c
@@ -11,15 +11,12 @@ printf("Introduzca su peso en kilogramos: ");
 scanf("%f", &peso);
 
 imc = peso / (altura*altura);
-char condicion = (18.5 > imc) ? "Bajo peso" :
-             (25 > imc) ? "Normal" : 
-             (30 > imc) ? "Sobrepeso" : 
-             "Obesidad";
-float descuento;
-descuento = (18.5 > imc) ? 0.05: (30 > imc) ? 0.1 : 0.15;
+char interpretacion = (18.5 > imc) ? "Bajo peso" : (25 > imc) ? "Normal" :  (30 > imc) ? "Sobrepeso" :  "Obesidad";
+float descuento = (18.5 > imc) ? 0.05: (30 > imc) ? 0.1 : 0.15;
 
-printf("Su IMC actual es: %.2f, y la interpretación es %s\n", imc, condicion);
-printf("Como incentivo, obtiene un descuento del %.0f%% en la tarifa de memebresia del gimnasio.", descuento*100);
+printf("Su IMC actual es: %.2f, y la interpretación es \n", imc);
+printf("%d", interpretacion);
+printf("Como incentivo, obtiene un descuento del %.0f%% en la tarifa de memebresia del gimnasio.\n", descuento*100);
 
     return 0;
 }
